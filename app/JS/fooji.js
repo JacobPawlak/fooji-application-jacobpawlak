264bb23a26e2e596bf87e465f0244060557f73c4
Jacob Pawlak <jacob.pawlak@uky.edu>
2017-04-04 14:44:03 -0400

    fooji application submission

--- app/JS/fooji.js
+++ app/JS/fooji.js
@@ -1,18 +1,9 @@
 (function() {
   $(document).ready(function() {
-    var state;
-    state = void 0;
     console.log('ready!');
-    state = 0;
     $('#fun-button').click(function() {
       console.log('Clicked');
-      if (state === 0) {
-        $(this).after('<div class=\'popup\'>You clicked it!</div>');
-        state = 1;
-      } else if (state === 1) {
-        $('.popup').remove();
-        state = 0;
-      }
+      $('#popup').toggleClass('.spooky');
     });
   });
 
